6326c1bb261dbf9b01dff3b0aa4db7cc17a73ba4
Andras Aszodi <aszodi@silverserver.at>
2012-02-21 10:21:49 +0100

    Figured out how to test randomplacer. It even passes...

--- src/file/test/randomplacertest.cc
+++ src/file/test/randomplacertest.cc
@@ -8,6 +8,10 @@
 using namespace multovl;
 using namespace prob;
 
+// -- Boost headers --
+
+#include "boost/foreach.hpp"
+
 // -- standard headers --
 
 #include <map>
@@ -17,27 +21,46 @@ using namespace prob;
 const double PCT_TOL=2.5;   // percentage tolerance for floating-point checks
 const unsigned int NTRIAL=100000;
 
+// NOTE: this could have been derived from FreeRegionsFixture
 struct RandomPlacerFixture
 {
     RandomPlacerFixture():
         rng(42u),    // uniform [0,1)
         total(0)
     {
-        frees.push_back(Region(100, 199, '+', "reg100"));  // len=100
-        frees.push_back(Region(200, 249, '+', "reg50"));  // len=50
-        frees.push_back(Region(300, 329, '+', "reg30"));  // len=30
-        frees.push_back(Region(400, 419, '+', "reg20"));  // len=20
+        // set up the free regions
+        frees.push_back(Region(100, 199, '+', "free100"));  // len=100
+        frees.push_back(Region(200, 249, '+', "free50"));  // len=50
+        frees.push_back(Region(300, 329, '+', "free30"));  // len=30
+        frees.push_back(Region(400, 419, '+', "free20"));  // len=20
         
-        counts["reg100"] = 0;
-        counts["reg50"] = 0;
-        counts["reg30"] = 0;
-        counts["reg20"] = 0;
+        counts["free100"] = 0;
+        counts["free50"] = 0;
+        counts["free30"] = 0;
+        counts["free20"] = 0;
+        
+        // these will fit in the "free100" and "free50", but not in the shorter ones
+        regs.push_back(Region(500, 539, '+', "reg40"));
+        regs.push_back(Region(600, 634, '+', "reg35"));
     }
     
-    void count(const std::string& regname)
+    void count(const Region& reg)
     {
-        counts[regname]++;
-        ++total;
+        unsigned int rf = reg.first(), rl = reg.last();
+        bool found = false;
+        BOOST_FOREACH(Region free, frees)
+        {
+            unsigned int ff = free.first(), fl = free.last();
+            if (ff <= rf && rl <= fl)
+            {
+                // within one of the free regions, found, hooray!
+                counts[free.name()]++;
+                ++total;
+                found = true;
+                break;
+            }
+        }
+        BOOST_CHECK_MESSAGE(found, "rf=" << rf << ",rl=" << rl);
     }
     
     double frac(const std::string& regname) const
@@ -47,30 +70,30 @@ struct RandomPlacerFixture
     }
     
     UniformGen rng;
-    std::vector<Region> frees;
+    std::vector<Region> frees, regs;
     std::map<std::string, unsigned int> counts;
     unsigned int total;
 };
 
 BOOST_FIXTURE_TEST_SUITE(randomplacersuite, RandomPlacerFixture)
 
-// no idea how to test this properly...
-#if 0
 BOOST_AUTO_TEST_CASE(randomplacer_test)
 {
-    const unsigned int MINLEN = 40;
-    
     FreeRegions fr(frees);
+    RandomPlacer rp(regs);
     for (unsigned int i=0; i<NTRIAL; ++i)
     {
-        std::string regname = fr.select_free_region(rng, MINLEN).name();
-        count(regname);
+        rp.random_placement(fr, rng);
+        BOOST_FOREACH(Region r, rp.get_regions())
+        {
+            count(r);
+        }
     }
-    BOOST_CHECK_CLOSE(frac("reg100"), 2.0/3.0, PCT_TOL);
-    BOOST_CHECK_CLOSE(frac("reg50"), 1.0/3.0, PCT_TOL);
-    BOOST_CHECK_CLOSE(frac("reg30"), 0.0, PCT_TOL);     // these are too short now...
-    BOOST_CHECK_CLOSE(frac("reg20"), 0.0, PCT_TOL);
+    BOOST_CHECK_EQUAL(total, 2*NTRIAL);   // make sure all shuffled regions fell in one of the frees...
+    BOOST_CHECK_CLOSE(frac("free100"), 2.0/3.0, PCT_TOL);
+    BOOST_CHECK_CLOSE(frac("free50"), 1.0/3.0, PCT_TOL);
+    BOOST_CHECK_CLOSE(frac("free30"), 0.0, PCT_TOL);     // these are too short now...
+    BOOST_CHECK_CLOSE(frac("free20"), 0.0, PCT_TOL);
 }
-#endif
 
 BOOST_AUTO_TEST_SUITE_END()
