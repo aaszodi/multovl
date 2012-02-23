6e1458b7cfdc46f7bbd718f1f53a81635256a3da
Andras Aszodi <aszodi@silverserver.at>
2012-02-23 06:35:54 +0100

    Started rewriting the empirdistr module using boost::accumulator tools. Can calculate min, max, mean, variance, took out the PDF and the inverse CDF, could not quite figure out how to retrieve CDF histogram values, needs probably some lower_bound tricks, so that is not tested yet.

--- src/file/empirdistr.hh
+++ src/file/empirdistr.hh
@@ -3,22 +3,20 @@
 
 // ==== HEADER empirdistr.hh ====
 
+// -- Boost headers --
+
+#include "boost/accumulators/accumulators.hpp"
+#include "boost/accumulators/statistics.hpp"
+using namespace boost::accumulators;
+
 // -- System headers --
-
-#include <math.h>
-
-#include <string>
-#include <list>
-#include <vector>
-
-// -- Library headers --
 
 namespace multovl {
 namespace prob {
 
 /// Objects of the EmpirDistr class implement univariate continuous empirical distributions
 /// that are approximated by histograms. Samples can be added to an EmpirDistr object
-/// and after data collection the approximate PDF, CDF and inverse CDF can be queried.
+/// and after data collection the approximate CDF can be queried.
 class EmpirDistr
 {
     // -- member classes
@@ -43,72 +41,21 @@ class EmpirDistr
     };
     // END of member class DirtyException
     
-    private:
-	    
-    /**
-     * A Bin object represents the i:th histogram bin.
-     * All bins are Width wide (see enclosing class for details).
-     */
-    class Bin
-    {
-        // methods
-        public:
-
-        /// Constructs the i:th bin (default 0)
-        explicit Bin(int i=0): _idx(i), _count(0) {}
-
-        /// Returns the index of the bin (may be negative).
-        int index() const { return _idx; }
-
-        /// \return the no. of samples in the bin.
-        unsigned int count() const { return _count; }
-
-        /// Comparison according to index
-        bool operator<(const Bin& other) { return index() < other.index(); }
-
-        /// Prefix increment: adds 1 to the bin count.
-        Bin& operator++() { ++_count; return *this; }
-
-        // data
-        private:
-
-        int _idx;	///< the bin index (can be negative)
-        unsigned int _count; 	///< no. of samples falling into the bin
-    };
-    // END of member class Bin
-    
-    // shorthands
-    typedef std::list<Bin> binlist_t;
-    typedef binlist_t::iterator biniter_t;
-    
     // methods
     public:
 	    
     /**
-     * Constructs an object so that it is ready to do an approximation
-     * with a bin width w.
-     * \param w the bin width. If w<0.0, then its absolute value is used.
+     * Constructs an object so that it is ready to do a CDF approximation
+     * with a number of cells
+     * \param ncell the number of cells, default 100. If ncell=0, then 100 is used.
      */
-    explicit EmpirDistr(double w) { reset(w); }
+    explicit EmpirDistr(unsigned int ncell = 0);
 
-    /**
-     * Resets the calling object so that it is ready to do an approximation
-     * with a bin width w. All previous data are lost.
-     * \param w the bin width. If w<0.0, then its absolute value is used.
-     * If w==0.0, then nothing happens.
-     */
-    void reset(double w);
-    
-    /**
-     * clears the calling object. All data are lost except the histogram bin width.
-     */
-    void clear();
-    
     /// adds a value 
     /// \param x a sample from the distribution to be approximated.
     EmpirDistr& add(double x);
     
-    /// evaluate(): normalise the counts and make the PDF and CDF.
+    /// evaluate(): normalise the counts and make the CDF.
     void evaluate();
     
     /**
@@ -134,31 +81,13 @@ class EmpirDistr
     }
     
     /**
-     * Interpolates the PDF.
-     * \param x the value at which the PDF is to be approximated.
-     * \return a linearly interpolated value for the PDF, or 0.0
-     * if /x/ lies outside [low()..high()].
-     * \throw DirtyException if evaluate() was not invoked beforehand.
-     */
-    double pdf(double x) const throw(DirtyException);
-    
-    /**
      * Interpolates the CDF.
      * \param x the value at which the CDF is to be approximated.
-     * \return a linearly interpolated value for the CDF, or 0.0
+     * \return an interpolated value for the CDF, or 0.0
      * if /x/ lies outside [low()..high()].
      * \throw DirtyException if evaluate() was not invoked beforehand.
      */
     double cdf(double x) const throw(DirtyException);
-    
-    /** 
-     * Interpolates the inverse CDF.
-     * \param y the probability value, must be in the range 0.0 .. 1.0
-     * \return an interpolated value for the inverse CDF, x where CDF(x)=y.
-     * Returns low() if y<=0, high() if y>=1.0
-     * \throw DirtyException if evaluate() was not invoked beforehand.
-     */
-    double inv_cdf(double Y) const throw(DirtyException);
     
     /**
      * \return the (estimated) mean of the distribution.
@@ -168,7 +97,7 @@ class EmpirDistr
     {
         if (_dirty)
             throw DirtyException("mean()");
-        return _mean;
+        return boost::accumulators::mean(_acc);
     }
     
     /**
@@ -179,27 +108,25 @@ class EmpirDistr
     {
         if (_dirty)
             throw DirtyException("variance()");
-        return _variance;
+        return boost::accumulators::variance(_acc);
     }
     
     private: 
-    // hidden methods
-    int bin_index(double x) const
-    {
-        return static_cast<int>(floor(x/_width + 0.5));
-    }
-    int locate_index(double x) const;
-    double bin_midpoint(int idx) const { return (_width*(idx+0.5)+_low); }
+    
+    typedef accumulator_set<double, 
+        features< 
+            tag::min, tag::max, 
+            tag::mean, tag::lazy_variance, 
+            stats<tag::p_square_cumulative_distribution>
+        >
+    > acc_t;
+    typedef boost::iterator_range<std::vector<std::pair<double, double> >::iterator > histogram_t;
+
     
     // data
-    binlist_t _binlist;     ///< histogram bins sorted in increasing center order
-    double _width;  ///< bin width
-    std::vector<double> _pdf;     ///< approximation of PDF
-    std::vector<double> _cdf;     ///< approximation of CDF
-    unsigned int _binno;     ///< length of Pdf and Cdf
-    double _low, _high;	    ///< lowest and highest X value covered by the bins
-    double _mean, _variance;  ///< estimated mean and variance
-    unsigned int _total;     ///< total number of X values added
+    acc_t _acc;
+    histogram_t _histogram;
+    double _low, _high;
     bool _dirty;     ///< true when a new value is added
     
 };
--- src/file/test/empirdistrtest.cc
+++ src/file/test/empirdistrtest.cc
@@ -49,25 +49,23 @@ BOOST_FIXTURE_TEST_SUITE(empirdistrsuite, EmpirDistrFixture)
 
 BOOST_AUTO_TEST_CASE(empty_test)
 {
-    EmpirDistr ed(1.0); // dirty state
+    EmpirDistr ed; // dirty state
     BOOST_CHECK_THROW(ed.low(), EmpirDistr::DirtyException);
     BOOST_CHECK_THROW(ed.high(), EmpirDistr::DirtyException);
     BOOST_CHECK_THROW(ed.mean(), EmpirDistr::DirtyException);
     BOOST_CHECK_THROW(ed.variance(), EmpirDistr::DirtyException);
-    BOOST_CHECK_THROW(ed.pdf(1.0), EmpirDistr::DirtyException);
     BOOST_CHECK_THROW(ed.cdf(0.5), EmpirDistr::DirtyException);
-    BOOST_CHECK_THROW(ed.inv_cdf(0.5), EmpirDistr::DirtyException);
 }
 
 // uniform distribution with values 1,2,..,6
 BOOST_AUTO_TEST_CASE(dice_test)
 {
-    EmpirDistr ed(0.1);
+    EmpirDistr ed;
     for (unsigned int i=0; i<dice.size(); ++i)
         ed.add(dice[i]);
     ed.evaluate();
-    BOOST_CHECK_CLOSE(ed.low(), 0.95, PCT_TOL); // within PCT_TOL %
-    BOOST_CHECK_CLOSE(ed.high(), 6.05, PCT_TOL);
+    BOOST_CHECK_CLOSE(ed.low(), 1.0, PCT_TOL); // within PCT_TOL %
+    BOOST_CHECK_CLOSE(ed.high(), 6.0, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.mean(), 3.5, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.variance(), 2.92, PCT_TOL);    // Mathematica says 7/2 though...
 }
@@ -81,8 +79,7 @@ BOOST_AUTO_TEST_CASE(unirnd_test)
     const double UNIMIN = 7.0, UNIMAX = 12.0, UNIWIDTH = UNIMAX-UNIMIN;
     UniformGen uni(42u, UNIMIN, UNIMAX);
 
-    const double BINWIDTH = 0.1;
-    EmpirDistr ed(BINWIDTH);
+    EmpirDistr ed;
     for (unsigned int i=0; i<10000; ++i)
     {
         double x = uni();
@@ -90,14 +87,13 @@ BOOST_AUTO_TEST_CASE(unirnd_test)
     }
     ed.evaluate();
     
-    BOOST_CHECK_CLOSE(ed.low(), UNIMIN-BINWIDTH, PCT_TOL);
-    BOOST_CHECK_CLOSE(ed.high(), UNIMAX+BINWIDTH, PCT_TOL);
+    BOOST_CHECK_CLOSE(ed.low(), UNIMIN, PCT_TOL);
+    BOOST_CHECK_CLOSE(ed.high(), UNIMAX, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.mean(), (UNIMIN+UNIMAX)/2.0, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.variance(), (UNIWIDTH*UNIWIDTH)/12.0, PCT_TOL);
     
-    // test PDF and CDF approximations
-    // at UNIMIN and UNIMAX positions, the PDF should be half of the uniform values
-    // within the interval due to the "trapezoidal" approximation, hence 0.5/UNIWIDTH
+    // test CDF approximations
+#if 0
     BOOST_CHECK_CLOSE(ed.pdf(UNIMIN), 0.5/UNIWIDTH, PCT_TOL);
     check_0("CDF(UNIMIN)", ed.cdf(UNIMIN));
     BOOST_CHECK_CLOSE(ed.inv_cdf(0.0), UNIMIN-BINWIDTH, PCT_TOL);
@@ -115,6 +111,7 @@ BOOST_AUTO_TEST_CASE(unirnd_test)
     BOOST_CHECK_CLOSE(ed.pdf(UNIMAX), 0.5/UNIWIDTH, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.cdf(UNIMAX), 1.0, PCT_TOL);
     BOOST_CHECK_CLOSE(ed.inv_cdf(1.0), UNIMAX+BINWIDTH, PCT_TOL);
+#endif
 }
 
 BOOST_AUTO_TEST_CASE(normrnd_test)
@@ -127,8 +124,7 @@ BOOST_AUTO_TEST_CASE(normrnd_test)
     normdistr_t normdistr(EMEAN, EDEV);
     boost::variate_generator<rng_t&, normdistr_t > norm(rng, normdistr);
 
-    const double BINWIDTH = 0.1;
-    EmpirDistr ed(BINWIDTH);
+    EmpirDistr ed;
     for (unsigned int i=0; i<10000; ++i)
     {
         double x = norm();
