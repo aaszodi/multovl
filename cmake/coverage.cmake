29b8440570e4576ffc60fbe4227cdccaeae3d353
Agyaras <agyaras@almacska.local>
2012-12-26 17:29:48 +0100

    Changed GCC coverage detection logic, fixed GCC V<4.2 coverage cmdline options

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -10,7 +10,7 @@
 # Variables that are set:
 # GCOV_CAPABLE_COMPILER: TRUE if the compiler supports gcov
 # CTEST_COVERAGE_COMMAND: /usr/bin/gcov or equivalent if gcov is installed, otherwise NOTFOUND
-# GCOV_COMPILER_FLAGS: flags needed for coverage analysis, "--coverage" for GNU C++
+# GCOV_COMPILER_FLAGS: flags needed for coverage analysis
 
 set(GCOV_CAPABLE_COMPILER FALSE)
 set(GCOV_COMPILER_FLAGS "")
@@ -20,44 +20,58 @@ if (NOT CTEST_COVERAGE_COMMAND)
     return()
 endif()
 
-if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    # Starting with Apple's XCode 4 (?), the stock llvm-g++ does NOT support coverage
-    # only "clang++" does
-    # we need to figure out the XCode version
-    execute_process(
-        COMMAND xcodebuild -version
-        COMMAND awk "BEGIN { FS=\"[ .]\" } /Xcode/ { print $2 }"
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE XCODEVER
-        ERROR_VARIABLE XCODEVER_error
-        RESULT_VARIABLE XCODEVER_result
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    if (NOT(XCODEVER_result EQUAL 0))
-        message(STATUS "Could not figure out XCode version, 4 assumed")
-        message(STATUS "[The problem was ${XCODEVER_error}]")
-        set(XCODEVER 4)
-    endif()
-    
-    if (XCODEVER EQUAL 4)
-        # Only clang++ can be used
-        if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-            set(GCOV_CAPABLE_COMPILER TRUE)
-        else()
-            message(STATUS "NOTE: The default llvm-g++ in XCode 4 cannot do coverage analysis.")
-            message(STATUS "NOTE: Use clang++ instead and/or complain to Apple!")
-        endif()
-    else()
-        # GNU and clang can be used
-        if (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-            set(GCOV_CAPABLE_COMPILER TRUE)
-        endif()
-    endif()
-elseif(CMAKE_COMPILER_IS_GNUCXX)
-    # assumed to work on other platforms
-    set(GCOV_CAPABLE_COMPILER TRUE)
+# Clang can do it
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+	set(GCOV_COMPILER_FLAGS "--coverage")
+	set(GCOV_CAPABLE_COMPILER TRUE)
+	return()
 endif()
 
-if (GCOV_CAPABLE_COMPILER)
-    set(GCOV_COMPILER_FLAGS "--coverage")
+# GCC can do it, but not on OSX with XCode 4 (??)
+if(CMAKE_COMPILER_IS_GNUCXX)
+
+	if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+		# Starting with Apple's XCode 4 (?), the stock llvm-g++ does NOT support coverage
+		# only "clang++" does
+		# we need to figure out the XCode version
+		execute_process(
+			COMMAND xcodebuild -version
+			COMMAND awk "BEGIN { FS=\"[ .]\" } /Xcode/ { print $2 }"
+			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+			OUTPUT_VARIABLE XCODEVER
+			ERROR_VARIABLE XCODEVER_error
+			RESULT_VARIABLE XCODEVER_result
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if (NOT(XCODEVER_result EQUAL 0))
+			message(STATUS "Could not figure out XCode version, 4 assumed")
+			message(STATUS "[The problem was ${XCODEVER_error}]")
+			set(XCODEVER 4)
+		endif()
+		
+		if (XCODEVER EQUAL 4)
+			message(STATUS "NOTE: The default llvm-g++ in XCode 4 cannot do coverage analysis.")
+			message(STATUS "NOTE: Use clang++ instead")
+		endif()
+	endif()
+
+	# Generic GCC or Apple's GCC with XCode younger than V4.0
+	# Detect GCC version
+	# http://stackoverflow.com/questions/4058565/check-gcc-minor-in-cmake
+	execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
+					OUTPUT_VARIABLE GCC_VERSION)
+	
+	# Looks like GCC below V4.2 needs "-fprofile-arcs -ftest-coverage"
+	# and "--coverage" later
+	set(GCC_MINIMAL_COVERAGE_VERSION "4.2")
+	if (GCC_VERSION VERSION_LESS GCC_MINIMAL_COVERAGE_VERSION)
+		set(GCOV_COMPILER_FLAGS "-fprofile-arcs -ftest-coverage")
+	else()
+		set(GCOV_COMPILER_FLAGS "--coverage")
+	endif()
+    set(GCOV_CAPABLE_COMPILER TRUE)
+    return()
 endif()
+
+
+
