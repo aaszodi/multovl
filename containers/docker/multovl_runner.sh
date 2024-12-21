ed3feb7e8d4876320448e8c83114577171a9c480
Andras Aszodi on Armdev <aszodi@interquadrat.eu>
2024-12-21 15:07:20 +0100

    Docker image build works on 'aarch64'

--- containers/docker/multovl_runner.sh
+++ containers/docker/multovl_runner.sh
@@ -7,14 +7,18 @@
 
 COMMANDS="anctrack.awk | bed2gff.sh | config_info | multovl | multovlprob | multovltest.sh | parmultovlprob"
 
+# the Multovl version is set as an env var in the Dockerfile
+TAG=${MULTOVL_VER}-$(uname -m)
+
 # -- Functions --
 
 print_help() {
     cat <<HELP
-Usage: docker run --rm --name multovl multovl:${MULTOVL_VERSION} <command> [<args>]
+Usage: docker run --rm --name multovl aaszodi/multovl:${TAG} <command> [<args>]
     where <command> is one of:
     ${COMMANDS}
     and <args> are optional arguments to the selected <command>
+Don't forget to (bind-)mount a directory containing the input and output files
 HELP
 }
 
